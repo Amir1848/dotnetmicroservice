b9176d158036e4597147b76ecad85e7df5a76716
AmirHesari <amir.hesari1848@gmail.com>
2023-09-06 13:03:02 +0330

    gateway

---
+++ MainGateway/MainGateway/Program.cs
@@ -0,0 +1,18 @@
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddOcelot();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+
+app.UseOcelot().Wait();
+app.Run();
