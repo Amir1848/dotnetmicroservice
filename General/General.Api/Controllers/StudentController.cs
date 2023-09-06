4c85e1dfd6d86d120ef8ccc89ac7cf97ad653918
AmirHesari <amir.hesari1848@gmail.com>
2023-09-06 08:30:21 +0330

    change

---
+++ General/General.Api/Controllers/StudentController.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using FUM.BaseApi;
+using General.Business;
+using General.Common;
+
+namespace General.Api.Controllers
+{
+    public class StudentController : FumControllerBase<Student, StudentViewModel, IStudentService>
+    {
+
+        public StudentController() { }
+    }
+}
